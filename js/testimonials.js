20c669dd47fcef372325a5fa5137156484a47224
pelumi adeyemi <pelumiadeyemi111@gmail.com>
2024-06-20 01:20:49 -0700

    Fixes on the testimonials section

--- js/testimonials.js
+++ js/testimonials.js
@@ -15,9 +15,8 @@ let people = [
 		photo:
 			'url("https://cdn.pixabay.com/photo/2018/03/06/22/57/portrait-3204843_960_720.jpg")',
 		name: "Susan Smith",
-		profession: "WEB DEVELOPER",
-		description:
-			"Cheese and biscuits chalk and cheese fromage frais. Cheeseburger caerphilly cheese slices chalk and cheese cheeseburger mascarpone danish fontina rubber cheese. Squirty cheese say cheese manchego jarlsberg lancashire taleggio cheese and wine squirty cheese. Babybel pecorino feta macaroni cheese brie queso everyone loves gouda. Cheese and biscuits camembert de normandie fromage fromage macaroni cheese"
+		profession: "WRITER",
+		description: "The AMAP course has been a game-changer for my career. The comprehensive curriculum and hands-on training provided me with the skills and confidence needed to excel in the field. The instructors are industry experts who genuinely care about their students' success. I highly recommend this course to anyone looking to advance their career."
 	},
 
 	{
@@ -25,8 +24,7 @@ let people = [
 			"url('https://cdn.pixabay.com/photo/2019/02/11/20/20/woman-3990680_960_720.jpg')",
 		name: "Anna Grey",
 		profession: "UFC FIGHTER",
-		description:
-			"I'm baby migas cornhole hell of etsy tofu, pickled af cardigan pabst. Man braid deep v pour-over, blue bottle art party thundercats vape. Yr waistcoat whatever yuccie, farm-to-table next level PBR&B. Banh mi pinterest palo santo, aesthetic chambray leggings activated charcoal cred hammock kitsch humblebrag typewriter neutra knausgaard. Pabst succulents lo-fi microdosing portland gastropub Banh mi pinterest palo santo"
+		description: "The AKDP course exceeded my expectations in every way. The detailed modules and real-world applications have significantly improved my knowledge and expertise. The support from the course instructors and the community is unparalleled. This course is a must for anyone serious about professional development."	
 	},
 
 	{
@@ -34,8 +32,15 @@ let people = [
 			"url('https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166_960_720.jpg')",
 		name: "Branson Cook",
 		profession: "ACTOR",
-		description:
-			"Radio telescope something incredible is waiting to be known billions upon billions Jean-François Champollion hearts of the stars tingling of the spine. Encyclopaedia galactica not a sunrise but a galaxyrise concept of the number one encyclopaedia galactica from which we spring bits of moving fluff. Vastness is bearable only through love paroxysm of global death concept"
+		description: "Enrolling in the CIIM course was one of the best decisions I've made. The course content is thorough and up-to-date with current industry trends. The interactive sessions and practical assignments have greatly enhanced my skills. I feel more equipped than ever to tackle challenges in my career. Highly recommended!"
+	},
+
+	{
+		photo:
+			"url('https://cdn.pixabay.com/photo/2014/10/30/17/32/boy-509488_960_720.jpg')",
+		name: "Julius Grohn",
+		profession: "PROFESSIONAL",
+		description: "The Ghost Writer course has transformed my writing abilities. The in-depth lessons and constructive feedback have helped me hone my craft and understand the intricacies of ghostwriting. The course structure is clear and engaging, making learning a pleasure. I now feel confident to take on ghostwriting projects professionally."
 	},
 
 	{
@@ -43,8 +48,15 @@ let people = [
 			"url('https://cdn.pixabay.com/photo/2014/10/30/17/32/boy-509488_960_720.jpg')",
 		name: "Julius Grohn",
 		profession: "PROFESSIONAL CHILD",
-		description:
-			"Biscuit chocolate pastry topping lollipop pie. Sugar plum brownie halvah dessert tiramisu tiramisu gummi bears icing cookie. Gummies gummi bears pie apple pie sugar plum jujubes. Oat cake croissant bear claw tootsie roll caramels. Powder ice cream caramels candy tiramisu shortbread macaroon chocolate bar. Sugar plum jelly-o chocolate dragée tart chocolate marzipan cupcake gingerbread."
+		description: "The AKDP course is a comprehensive program that covers all the necessary aspects to excel in the field. The interactive learning environment and the supportive instructors made the experience enjoyable and enriching. I've learned so much and feel more confident in my abilities. This course is a fantastic investment in your future."
+	},
+
+	{
+		photo:
+			"url('https://cdn.pixabay.com/photo/2014/10/30/17/32/boy-509488_960_720.jpg')",
+		name: "Julius Grohn",
+		profession: "CHILD",
+		description: "The AMAP course provided me with invaluable insights and practical knowledge. The course is well-organized, and the instructors are passionate and knowledgeable. The skills I've gained have already started to pay off in my job. I couldn't be happier with my decision to enroll in this course."
 	}
 ];
 
